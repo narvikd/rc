519c7c2fb1e9227c5079a221972c44ad246122bd
narvikd <84069271+narvikd@users.noreply.github.com>
2022-04-17 11:32:45 +0200

    Added go-rocket-update for POC self-updater
    Signed-off-by: narvikd <84069271+narvikd@users.noreply.github.com>

--- cmd/root.go
+++ cmd/root.go
@@ -1,13 +1,62 @@
 package cmd
 
 import (
+	"fmt"
+	"github.com/mouuff/go-rocket-update/pkg/provider"
+	"github.com/mouuff/go-rocket-update/pkg/updater"
 	"github.com/spf13/cobra"
 	"os"
+	"rc/pkg/fmtutils"
+	"runtime"
 )
+
+var AppVersion = "0.0.2"
+var showVersion bool
+var tryUpdate bool
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use: "rc",
+	Run: func(cmd *cobra.Command, args []string) {
+		if showVersion {
+			handleShowVersion()
+		}
+
+		if tryUpdate {
+			handleUpdate()
+		}
+
+		_ = cmd.Help()
+	},
+}
+
+func handleShowVersion() {
+	defer os.Exit(0)
+	fmt.Println("Current version: " + AppVersion)
+}
+
+func handleUpdate() {
+	defer os.Exit(0)
+	u := &updater.Updater{
+		Provider: &provider.Github{
+			RepositoryURL: "https://github.com/narvikd/rc",
+			ArchiveName:   fmt.Sprintf("binaries_%s.zip", runtime.GOOS),
+		},
+		ExecutableName: fmt.Sprintf("rc_%s_%s", runtime.GOOS, runtime.GOARCH),
+		Version:        AppVersion,
+	}
+
+	fmt.Println("Current version: " + u.Version)
+	fmt.Println("Looking for updates...")
+
+	status, err := u.Update()
+	if err != nil {
+		fmtutils.FatallnMsg("Couldn't update. Error: " + err.Error())
+	}
+
+	if status == updater.Updated {
+		fmt.Println("RC found a new version and updated itself, please restart.")
+	}
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -21,4 +70,8 @@ func Execute() {
 
 func init() {
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
+	rootCmd.Flags().BoolVarP(&showVersion, "version", "v", false,
+		"Shows current rc version.")
+	rootCmd.Flags().BoolVarP(&tryUpdate, "update", "u", false,
+		"Updates rc version to the latest version.")
 }
--- go.mod
+++ go.mod
@@ -4,10 +4,12 @@ go 1.18
 
 require (
 	github.com/TwiN/go-color v1.1.0
+	github.com/mouuff/go-rocket-update v1.5.2
 	github.com/spf13/cobra v1.4.0
 )
 
 require (
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )
